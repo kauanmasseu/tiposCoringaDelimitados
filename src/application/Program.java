17c86ee14d518bfefe5d1b2e06c086cbe9e815c8
Kauan Masseu <masseukauan7@gmail.com>
2025-12-15 15:52:41 -0300

    principio get/put - covariancia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,21 @@
 package application;
 
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
+
     public static void main(String[] args) {
 
-        List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Rectangle(3.0, 2.0));
-        myShapes.add(new Circle(2.0));
+        List<Integer> intList = new ArrayList<>();
+        intList.add(10);
+        intList.add(5);
 
-        List<Circle> myCircles = new ArrayList<>();
-        myCircles.add(new Circle(2.0));
-        myCircles.add(new Circle(3.0));
+        //Covariancia, aceita acessar a lista mas não aceita adicionar nada na lista.
+        List<? extends Number> list = intList;
 
-        System.out.println("Total area: " + totalArea(myCircles));
-    }
+        Number x = list.get(0);
 
-    //retorna uma lista de qualquer tipo que seja subclasse de Shape
-    public static double totalArea(List<? extends Shape> list) {
-        double sum = 0;
-        for(Shape s : list) {
-            sum += s.area();
-        }
-        return sum;
+        list.add(20);
     }
 }
